e68f5d37cb4633b09912b55f1ecdda1de8572ed1
yroussea <youenn.rousseau@hotmail.com>
2024-03-11 16:58:36 +0100

    readline

--- src/minishell.c
+++ src/minishell.c
@@ -3,6 +3,7 @@
 
 int	main(int argc, char **argv, char **envp)
 {
+	ft_printf("%s", readline());
 	(void)argc;
 	(void)argv;
 	(void)envp;
--- src/minishell.h
+++ src/minishell.h
@@ -4,5 +4,8 @@
 # include "libft/libft.h"
 # include "struct.h"
 # include "function.h"
+# include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
 #endif
