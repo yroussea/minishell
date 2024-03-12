e566d38ef0462a773ba640fae30610f7c21210b6
yroussea <youenn.rousseau@hotmail.com>
2024-03-12 17:28:57 +0100

    while dans le main

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: yroussea <yroussea@student.42angouleme.fr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 17:06:41 by yroussea          #+#    #+#             */
-/*   Updated: 2024/03/12 17:23:47 by yroussea         ###   ########.fr       */
+/*   Updated: 2024/03/12 17:28:21 by yroussea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,18 @@ int	main(int argc, char **argv, char **envp)
 
 	if (argc != 1)
 		return (1);
-	line = readline(">");
-	lst_line = parsing(line);
-	ft_print_lst(2, lst_line);
-	free(line);
 	lst_envp = init_lst_envp(envp);
-
 	char	**en = envp_to_char(lst_envp);
 	ft_printf("%S", en);
 	free_lst_envp(lst_envp);
+
+	while (1)
+	{
+		line = readline(">");
+		lst_line = parsing(line);
+		ft_print_lst(2, lst_line);
+		free(line);
+	}
 
 	(void)argc;
 	(void)argv;
