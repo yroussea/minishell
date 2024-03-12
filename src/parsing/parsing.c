286f7ce19b41ba6898a551df80e3e35eb7fb641a
yroussea <youenn.rousseau@hotmail.com>
2024-03-12 19:51:38 +0100

    fix leak parsing

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: yroussea <yroussea@student.42angouleme.fr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 17:25:08 by yroussea          #+#    #+#             */
-/*   Updated: 2024/03/12 19:13:49 by yroussea         ###   ########.fr       */
+/*   Updated: 2024/03/12 19:51:02 by yroussea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ char	**split_line(char *line)
 
 t_type_of_node	get_type(char *s)
 {
-	if (ft_strncmp(s, "||", 3))
+	if (ft_strncmp(s, "||", 3) == 0)
 		return (OR);
-	if (ft_strncmp(s, "|", 2))
+	if (ft_strncmp(s, "|", 2) == 0)
 		return (PIPE);
-	if (ft_strncmp(s, "&&", 3))
+	if (ft_strncmp(s, "&&", 3) == 0)
 		return (AND);
 	return (CMD);
 }
@@ -56,6 +56,7 @@ t_lst_cmd	*parsing(char *line)
 		type = get_type(args[i]);
 		if (!ft_lst_cmd_add(&lst_cmd, unzoom(args[i]), type))
 		{
+			ft_lst_cmd_free(lst_cmd);
 			ft_magic_free("%2 %1", args, line);
 			return (NULL);
 		}
