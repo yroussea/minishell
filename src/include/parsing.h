ba795e62e1bba0a2c3f82ca36b690e444d07e473
yroussea <youenn.rousseau@hotmail.com>
2024-03-15 18:14:52 +0100

    merge .h

--- src/include/parsing.h
+++ src/include/parsing.h
@@ -1,17 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parting.h                                          :+:      :+:    :+:   */
+/*   parsing.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: basverdi <basverdi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yroussea <yroussea@student.42angouleme.fr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 20:59:05 by yroussea          #+#    #+#             */
-/*   Updated: 2024/03/15 18:08:20 by basverdi         ###   ########.fr       */
+/*   Updated: 2024/03/15 18:14:20 by yroussea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef parsing_H
 # define parsing_H
+
+typedef enum s_quote
+{
+	NONE,
+	SIMPLE,
+	DOUBLE,
+}			t_quote;
 
 char		**va_tokeniser(char *s, int nb, ...);
 t_lst_cmd	*parsing(char *line);
