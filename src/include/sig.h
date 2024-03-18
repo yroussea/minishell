98761a29a5920714b10d1f6f5de3ef4b2c85bfbe
basverdi <basverdi@3F2.42angouleme.fr>
2024-03-18 16:55:41 +0100

    sig.h

--- src/include/sig.h
+++ src/include/sig.h
@@ -1,24 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   function.h                                         :+:      :+:    :+:   */
+/*   sig.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: basverdi <basverdi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/11 15:12:07 by yroussea          #+#    #+#             */
-/*   Updated: 2024/03/18 16:55:19 by basverdi         ###   ########.fr       */
+/*   Created: 2024/03/18 16:54:24 by basverdi          #+#    #+#             */
+/*   Updated: 2024/03/18 16:54:55 by basverdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FUNCTION_H
-# define FUNCTION_H
+#ifndef SIG_H
+# define SIG_H
 
-# include "struct.h"
-
-# include "utils.h"
-# include "parsing.h"
-# include "cmd.h"
-# include "exec.h"
-# include "sig.h"
+void	set_sigaction(int state);
 
 #endif
