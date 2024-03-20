d5e67b2eb20ca8910c333ec6badc77e8cde7261a
yroussea <youenn.rousseau@hotmail.com>
2024-03-20 16:57:34 +0100

    exectree/

--- src/exec/exectree/exec_tree.c
+++ src/exec/exectree/exec_tree.c
@@ -6,7 +6,7 @@
 /*   By: basverdi <basverdi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 15:50:00 by yroussea          #+#    #+#             */
-/*   Updated: 2024/03/18 17:53:21 by basverdi         ###   ########.fr       */
+/*   Updated: 2024/03/20 16:57:17 by yroussea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ t_bool	exec_pipe(t_node *node, t_lst_envp *envp, t_stack_id *stk_pid, t_fds fds)
 	fds.in = fd_pipe[0];
 	fds.out = 1;
 	exec_tree(node->right, envp, stk_pid, fds);
+	//wait la pipeline?
 	if (ft_close(2, fd_pipe[0], fd_pipe[1]) < 0)
 		return (ERROR);
 	return (TRUE);
@@ -32,6 +33,15 @@ t_bool	exec_pipe(t_node *node, t_lst_envp *envp, t_stack_id *stk_pid, t_fds fds)
 
 t_bool	exec_and(t_node *node, t_lst_envp *envp, t_stack_id *stk_pid, t_fds fds)
 {
+	/*(si dans pipe faire la suite dans un fork puis exit)
+	 * dup2 entrer (si pas STDIN)
+	 * exectree pour la gauche
+	 * recup depuis la pile le pid
+	 * close les pipes
+	 * wait le fils
+	 * si le fils a reussit (global ig)
+	 * ->executer a droite
+	 */
 	(void)node;
 	(void)envp;
 	(void)stk_pid;
@@ -41,6 +51,15 @@ t_bool	exec_and(t_node *node, t_lst_envp *envp, t_stack_id *stk_pid, t_fds fds)
 
 t_bool	exec_or(t_node *node, t_lst_envp *envp, t_stack_id *stk_pid, t_fds fds)
 {
+	/*(si dans pipe faire la suite dans un fork puis exit)
+	 * dup2 entrer (si pas STDIN)
+	 * exectree pour la gauche
+	 * recup depuis la pile le pid
+	 * close les pipes
+	 * wait le fils
+	 * si le fils a echouer (global ig)
+	 * ->executer a droite
+	 */
 	(void)node;
 	(void)envp;
 	(void)stk_pid;
@@ -53,6 +72,7 @@ t_bool	exec_cmd(t_node *node, t_lst_envp *envp, t_stack_id *stk_pid, t_fds fds)
 	int		pid;
 	char	**envp_char;
 //difference buildin et cmd
+//node->cmd should be all path cmd
 	pid = ft_fork();
 	if (pid < 0)
 		return (ERROR);
@@ -61,12 +81,18 @@ t_bool	exec_cmd(t_node *node, t_lst_envp *envp, t_stack_id *stk_pid, t_fds fds)
 		//all redirection
 		envp_char = envp_to_char(envp);
 		if (envp_char)
+		{
+			//remplirer cmd avec full path
+			//make all redir (dup2)
+			//close all pipes
+			//close all redir (carefull if no redir)
 			execve(node->cmd, node->args, envp_char);
+		}
 		free(envp_char);
 		exit(1);
 	}
+	//close cmd redir
 	//stack_id_add(pid)
-	waitpid(pid, NULL, 0);
 	(void)stk_pid;
 	(void)fds;
 	return (ERROR);
