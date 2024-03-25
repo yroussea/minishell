de5e85fe2a22c20f11efa1b7c722df41596a5ce0
yroussea <youenn.rousseau@hotmail.com>
2024-03-25 15:03:30 +0100

    v2 du unquoting

--- src/parsing/unquoting.c
+++ src/parsing/unquoting.c
@@ -6,7 +6,7 @@
 /*   By: yroussea <yroussea@student.42angouleme.fr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 12:06:03 by yroussea          #+#    #+#             */
-/*   Updated: 2024/03/25 14:17:15 by yroussea         ###   ########.fr       */
+/*   Updated: 2024/03/25 15:00:18 by yroussea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,12 +61,14 @@ char	*ft_undoublequote(char *s, t_lst_envp *lst_envp)
 		s++;
 	if (*s == 36)
 	{
-		tmp = ft_undolars(s++, lst_envp);
+		tmp = ft_undolars(++s, lst_envp);
 		while (s && *s && *s != 34 && *s != 36 && *s != 39)
 			s++;
-		return (ft_vjoin(3, res, tmp, ft_undoublequote(s, lst_envp)));
+		return (ft_vjoin(3, "", res, tmp, ft_undoublequote(s, lst_envp)));
 	}
-	return (ft_vjoin(2, "", res, ft_undolars(s, lst_envp)));
+	if (s && *s && *(s + 1))
+		return (ft_vjoin(2, "", res, ft_unquote(s + 1, lst_envp)));
+	return (res);
 }
 
 char	*ft_undolars(char *s, t_lst_envp *lst_envp)
@@ -74,7 +76,6 @@ char	*ft_undolars(char *s, t_lst_envp *lst_envp)
 	char	*res;
 	char	*variable;
 
-//	res = strdup_until_sep(s, 3, 34, 36, 39);
 	res = strdup_until_sep(s, 3, 34, 36, 39);
 	variable = get_envp_variable(lst_envp, res);
 	free(res);
@@ -88,7 +89,7 @@ char	*ft_unsimplequote(char *s, t_lst_envp *lst_envp)
 	res = strdup_until_sep(s, 1, 39);
 	while (s && *s && *s != 39)
 		s++;
-	return (ft_vjoin(2, "", res, ft_unquote(s, lst_envp)));
+	return (ft_vjoin(2, "", res, ft_unquote(s + 1, lst_envp)));
 }
 
 char	*ft_unquote(char *s, t_lst_envp *lst_envp)
@@ -98,11 +99,12 @@ char	*ft_unquote(char *s, t_lst_envp *lst_envp)
 	res = strdup_until_sep(s, 3, 34, 36, 39);
 	while (s && *s && *s != 34 && *s != 36 && *s != 39)
 		s++;
-	if (*s == 34)
-		return (ft_vjoin(2, "", res, ft_undoublequote(s, lst_envp)));
-	if (*s == 36)
-		return (ft_vjoin(2, "", res, ft_undolars(s, lst_envp)));
-	if (*s == 39)
-		return (ft_vjoin(2, "", res, ft_unsimplequote(s, lst_envp)));
+	ft_printf_fd(2, "#A->%s--%s\n",s,res);
+	if (s && *s == 34)
+		return (ft_vjoin(2, "", res, ft_undoublequote(s + 1, lst_envp)));
+	if (s && *s == 36)
+		return (ft_vjoin(2, "", res, ft_undolars(s + 1, lst_envp)));
+	if (s && *s == 39)
+		return (ft_vjoin(2, "", res, ft_unsimplequote(s + 1, lst_envp)));
 	return (res);
 }
