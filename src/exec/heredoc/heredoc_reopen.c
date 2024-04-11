23caa48b83c07ed04290c968bfa12ff08ea9f07d
Youenn Rousseau <yroussea@2B6.42angouleme.fr>
2024-04-11 16:31:11 +0200

    reopen heredoc

---
+++ src/exec/heredoc/heredoc_reopen.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heredoc_reopen.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yroussea <yroussea@student.42angouleme.fr  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/11 12:57:07 by yroussea          #+#    #+#             */
+/*   Updated: 2024/04/11 13:52:06 by yroussea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../minishell.h"
+
+static char	ft_random(void)
+{
+	char	buf[1];
+	int		fd;
+
+	fd = open("/dev/random", 0);
+	read(fd, buf, 1);
+	ft_close(1, fd);
+	return (97 + ft_abs(*buf % 26));
+}
+
+char	*replace_dollar(char *s, t_lst_envp *lst_envp)
+{
+	char	*result;
+	char	*str;
+	char	*tmp;
+	char	*tmp_str;
+
+	result = strdup_until_sep(s, 1, 36);
+	while (s && *s && *s != 36)
+		s++;
+	if (s && *s == 36)
+	{
+		str = ft_undolars(++s, lst_envp);
+		while (s && *s && *s != 34 && *s != 36 && \
+			*s != 39 && *s != ' ' && *s != '\n')
+			s++;
+		tmp_str = replace_dollar(s, lst_envp);
+		tmp = ft_vjoin(3, "", result, str, tmp_str);
+		ft_magic_free("%1 %1 %1", result, str, tmp_str);
+		return (tmp);
+	}
+	return (result);
+}
+
+t_bool	fill_heredoc(int old_fd, int new_fd, t_lst_envp *lst_envp)
+{
+	char	*buf;
+	char	*str;
+	int		r;
+
+	r = 1;
+	str = NULL;
+	while (r)
+	{
+		buf = malloc(100 + 1);
+		r = read(old_fd, buf, 100);
+		if (r == -1)
+		{
+			free(str);
+			free(buf);
+			return (FALSE);
+		}
+		buf[r] = 0;
+		str = ft_vjoin(2, "", str, buf);
+		free(buf);
+	}
+	buf = replace_dollar(str, lst_envp);
+	free(str);
+	ft_printf_fd(new_fd, "%s", buf);
+	return (TRUE);
+}
+
+int	heredoc_reopen(int fd, t_lst_envp *lst_envp)
+{
+	int		new_fd;
+	char	buf[19];
+	int		i;
+
+	ft_strlcpy(buf, ".heredoc", 9);
+	i = 8;
+	while (i < 18)
+		buf[i++] = ft_random();
+	buf[i] = 0;
+	new_fd = open((char *)buf, 577, 0664);
+	if (!fill_heredoc(fd, new_fd, lst_envp))
+	{
+		unlink(buf);
+		close(!new_fd);
+		return (fd);
+	}
+	close(fd);
+	close(new_fd);
+	new_fd = open((char *)buf, 0);
+	unlink(buf);
+	return (new_fd);
+}
